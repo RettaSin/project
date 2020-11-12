87583505d8c88bf71c2ea730da43d65d05e52589
aAaA <themargaret.play@gmail.com>
2020-11-13 00:10:06 +0300

    lol

--- projekt/src/com/company/Main.java
+++ projekt/src/com/company/Main.java
@@ -1,15 +1,41 @@
 package com.company;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
+    public static void stat(){
+     int strenght = 3;
+     int HP = 100;
+     String mind = "Состояние стабильно";
+     int patrons = 0;
+     String golod = "Вы сыты";
+     String invent = "На данный момент в рюкзаке пусто";
+    String mest = "Ваше местоположение: Хабаровск, подвал";
+        System.out.println("Ваша сила: "+strenght);
+        System.out.println("Вы абсолютно здоровы");
+        System.out.println(mind);
+        System.out.println("У вас нет продажных патронов");
+        System.out.println(golod);
+        System.out.println(invent);
+        System.out.println(mest);
+    }
 
-    public int strenght = 1;
-    public int HP = 100;
-    public String mind = "Состояние стабильно";
-    public int patrons = 0;
-    public String golod = "Вы сыты";
-    public String invent = "На данный момент в рюкзаке пусто";
+     public static void nastr(){
+        System.out.println("Смена имени");
+        System.out.println("Введите имя:");
+        Scanner nam = new Scanner(System.in);
+        String mn = nam.nextLine();
+        System.out.println("Вы успешно сменили имя, "+mn);
+     }
+
+     public static void dostij(){
+        System.out.println("Достижения: 'Пролог' - согласитесь прочитать пролог !");
+     }
+
+    public static void nodostij(){
+        System.out.println("Достижения: 'Ленивец' - не согласитесь прочитать пролог и закончите игру!");
+    }
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -30,22 +56,12 @@ public class Main {
         System.out.println("2 ");
         Thread.sleep(1000);
         start();
-        System.out.println("Желаете выбрать имя сейчас? Это всегда можно будет сделать, написав \"настройки\" ");
-        System.out.println("Базовое имя будет выбрано взависимости от пола");
-        System.out.println("Или Алексей, или Екатерина.");
-        System.out.println("1. Желаю. 2. Нет");
-        Scanner c = new Scanner(System.in);
-        int ded = c.nextInt();
-        if (ded == 1){
-            Scanner name = new Scanner(System.in);
-            String na = name.nextLine();
-            System.out.println("Очень красивое имя, "+na);
-            star();
-        }
-        if (ded == 2)
-        {
-            star();
-        }
+
+
+
+
+
+
 
 
 
@@ -70,7 +86,67 @@ public class Main {
         System.out.println("(Настоятельно рекомендуем для большего ознакомления с сюжетом) ");
         System.out.println("1. Да. 2. Нет");
 
+        Scanner blya = new Scanner(System.in);
+        String k = blya.nextLine();
+        if(k.equals("1")) {
+            System.out.println("Шел 2046 год. Холодная война переросла в атомную и в конце концов многие люди погибли вначале при взрыве бомбы ");
+            System.out.println("А остальные и от ядерной зимы, которая наступила вследствии\n" +
+                    "Не сказать что многие выжили, но кто смог, те постарались приспособиться по максимуму ");
+            System.out.println("Чудо, что вы не погибли. Кто знает, как сложилась бы ваша судьба, не вскопай ваш отец бункер еще в 85, стоило почуять беду\n" +
+                    "Вышел он добротным, хоть и не очень большим. ");
+            System.out.println("Спальник вы организовали на очень большой полке, на которой раньше ютились банки с соленьями ");
+            System.out.println("Сейчас тут в основном вода и еда, которую удалось достать. С питанием был дефецит, для этого нужно было выбираться на поверхность ");
+            System.out.println("Помимо ужасной жары и нехватки воздуха вас там еще и ожидали мутанты, да и другие люди ");
+            System.out.println("Район пустынный, так что на случайных прохожих не надейтесь, это бандиты которые вытуривают людей с их жилища ");
+            System.out.println("В отличии от вас, у них есть машины. Зато у вас есть хороший вентилятор и темпорегулятор. ");
+            System.out.println("Вы могли бы сказать что до сих пор помните день сброса бомбы, но вас тогда еще не было на свете ");
+            System.out.println("Чудо, что вы вообще появились и нормально выросли, настоящее чудо, но удивляться вскоре стало некому. ");
+            System.out.println("Для вас такая жизнь - обыденность. Светлой памяти о былых деньках вам взять неоткуда. ");
+            System.out.println("Конечно, можно было бы дойти до убежищ с гражданскими, но в последнее время туда принимают только военных.");
+            System.out.println("Остается лишь жить в собственной комнатке без социума. ");
+            System.out.println(" Поздравляем, Достижение 'Пролог'");
+            System.out.println("Посмотреть достижение можно введя после 'достиж'");
+            System.out.println("Вы остановились на мысли, что вам показался каким-то странным и посторонним шум. ");
+            System.out.println("Стоит ли пойти проверить? ");
+            System.out.println("1. Да, стоит. 2. Прислушаться. 3. Забить");
+            System.out.println("Спасибо за тест игры!");
+            System.out.println("Теперь вы можете проверить функции: статы, настройки, достиж ");
+            cyc();
+
+
+        }
+        if (k.equals("2"))
+        {
+            System.out.println("Пф ну и досвидос бета версия окончена");
+            System.out.println("Можете ввести достиж и посмотреть на свои заслуги лох");
+            System.out.println("Так же проверить функции: статы, настройки");
+            cyc();
+        }
+
+        if (k.equals("настройки")){ nastr();}
+        if (k.equals("статы")) {stat();};
+
+
+
     }
+
+    private static void cyc() {
+        Scanner a = new Scanner(System.in);
+        String b = a.nextLine();
+        if (b.equals("настройки")) {
+            nastr();
+            cyc();
+        }
+        if (b.equals("статы")) {
+            stat();
+            cyc();
+        }
+        if (b.equals("достиж")) {
+            dostij();
+            cyc();
+        }
+    }
+
     public static void start() {
         Scanner in = new Scanner(System.in);
         System.out.println("Попробуй!");
@@ -102,6 +178,22 @@ public class Main {
 
             }
             System.out.println("Поздравляю, вы успешно выбрали пол!");
+            System.out.println("Желаете выбрать имя сейчас? Это всегда можно будет сделать, написав \"настройки\" ");
+            System.out.println("Базовое имя будет выбрано взависимости от пола");
+            System.out.println("Или Алексей, или Екатерина.");
+            System.out.println("1. Желаю. 2. Нет");
+            Scanner c = new Scanner(System.in);
+            String ded = c.nextLine();
+            if (ded.equals("1")){
+                Scanner name = new Scanner(System.in);
+                String na = name.nextLine();
+                System.out.println("Очень красивое имя, "+na);
+                star();
+            }
+            if (ded.equals("2"))
+            {
+                star();
+            }
 
 
 
